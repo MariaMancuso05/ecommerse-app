702bb1f8d11c2fb906d6867830f71415297a1440
MariaMancuso05 <mariamancuso219@gmail.com>
2025-07-16 14:39:49 +0200

    fix checkout bug for large carts

--- eccommerce-app/checkout.js
+++ eccommerce-app/checkout.js
@@ -1,1 +1,9 @@
-function checkout() { /* v1.0 logic */ }
+function checkout() 
+{
+/* v1.0 logic - FIXED for large carts */
+if (cart.total > 100){
+	return processLargeOrder();
+}
+return processStandardOrder();
+}
+
